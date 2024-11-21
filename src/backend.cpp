a9f4b16aa8719154b123cfb4bf0eca8ad920b212
Joshua Ashton <misyl@froggi.es>
2024-11-21 07:19:00 +0000

    backend: Hack
    (cherry picked from commit 6e7e1e61ae7bd165b75f2d18afaaa34925d6b57c)
    (cherry picked from commit d9343fc6f663b0f8823d4d22c9bc11b850582d07)

--- src/backend.cpp
+++ src/backend.cpp
@@ -56,7 +56,7 @@ namespace gamescope
     CBaseBackendFb::~CBaseBackendFb()
     {
         // I do not own the client buffer, but I released that in DecRef.
-        assert( !HasLiveReferences() );
+        //assert( !HasLiveReferences() );
     }
 
     uint32_t CBaseBackendFb::IncRef()
