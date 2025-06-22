6a56ce18fcf87d582e17901164d28b932dbd7a5f
Matthew Schwartz <matthew.schwartz@linux.dev>
2025-06-22 10:39:19 -0700

    steamcompmgr: track FSR state with preemptive upscaling
    g_bFSRActive was only being applied to the first frame of preemptive
    upscaling, which meant that the FSR badge would quickly flicker from on
    to off when preemptive upscaling was active.
    
    To account for this, track the state of preemptive upscaling and use
    it to activate g_bFSRActive when applicable.
    
    (cherry picked from commit 81e40911e425c41071f3f684eba76b154b25f7af)

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -2634,6 +2634,9 @@ paint_all(bool async)
 	}
 
 	g_bFSRActive = frameInfo.useFSRLayer0;
+	if ( const auto& heldCommit = g_HeldCommits[HELD_COMMIT_BASE]; heldCommit && heldCommit->upscaledTexture ) {
+		g_bFSRActive = ( heldCommit->upscaledTexture->eFilter == GamescopeUpscaleFilter::FSR );
+	}
 
 	g_bFirstFrame = false;
 
